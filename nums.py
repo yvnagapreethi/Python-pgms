2fb5b97f7281de3f7b1f31c6d7ba2d3ef74c69b8
Preethi <Venkata_Naga_Preethi@Dell.com>
2019-01-30 13:49:31 -0500

    add linkedlist

--- nums.py
+++ nums.py
@@ -4,8 +4,10 @@ class Numbers:
         self.nums = input("enter the numbers seperated by comma :")
         self.list_tuple(self.nums)
 
-    def list_tuple(self,nums):
+    def list_tuple(self, nums):
         self.lst = self.nums.split(",")
-        print("List is {0} and tuple is {1} :".format(self.lst,tuple(self.lst)))
+        print("List {0} tuple {1} :".format(self.lst, tuple(self.lst)))
 
-nm = Numbers()
+
+if __name__ == '__main__':
+    n = Numbers()
--- printnames.py
+++ printnames.py
@@ -1,8 +1,10 @@
 class PrintNames:
-    
+
     def __init__(self):
         first = input("Enter your first name")
         last = input("Enter your last name")
-        print("Final is {0} {1}".format(first[-1::-1],last[-1::-1]))
+        print("Final is {0} {1}".format(first[-1::-1], last[-1::-1]))
 
-pn = PrintNames()
+
+if __name__ == '__main__':
+    pn = PrintNames()
