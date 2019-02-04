62edeab20d9c7322939d94c0d747f4b3abbc7362
Preethi <yvnagapreethi@gmail.com>
2019-02-04 06:38:19 -0500

    areaofcirlce and printnames files modification

--- areaofcircle.py
+++ areaofcircle.py
@@ -1,12 +1,12 @@
-class AreaOfCirlce:
+class Cirlce:
 
     def __init__(self):
         self.radius = float(input("Please enter the radius value to find the area of a cirlce"))
         print("The area of a circle is:{0} ".format(self.area1(self.radius)))
 
-    def area1(self,radius):
-        return 3.14*self.radius**2
+    def area1(self, radius):
+        return 3.14 * self.radius**2
         # print("The radius is {0}".format(radius))
 
 
-ar = AreaOfCirlce()
+Cirlce()
--- printnames.py
+++ printnames.py
@@ -3,7 +3,10 @@ class PrintNames:
     def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
-        print("Full Name is {0} {1}".format(self.first_name[-1::-1], self.last_name[-1::-1]))
+        print(self.print_complete_name())
+
+    def print_complete_name(self):
+        return "Full Name is {0} {1}".format(self.first_name[-1::-1], self.last_name[-1::-1])
 
 
 if __name__ == '__main__':
